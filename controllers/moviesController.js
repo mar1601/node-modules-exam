f485af549fef09331ba0d88eb70bfa2081342797
mar1601 <mgarcia2@institutmvm.cat>
2021-12-10 18:12:47 +0100

    finalitzat

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -16,38 +16,65 @@ import actorPojo from '../models/actorPojo.js';
 */
 
 const getAllMovies = (data_movie) => {
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+    
+    const movie = moviesModel.getMovieBy(data_movie.req)
+    
+    if (typeof movie == "undefined"){
+        throw new Error("Error");
+    }
+    data_movie.res = movie;
+
 }
 
 const getMovieById = (data_movie) => {
-    // ...
-
+    if(!req.id) throw new Error('parametro Id no existe')
+   
+    const movie = movieModels.getMovieId(req.id);
+   
+    if(typeof movie == 'undefined'){
+        throw new Error ('UPS! Pelicula no encontrada')
+    };
+   
+    res.push(movie);
+    return res;
 }
 
 const removeMovie = (data_movie) => {
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+
+    res = movieModels.removeMovie(req.id);
+    return res;
 }
 
 const createMovie = (data_movie) => {
     // Puede usar ../models/moviePojo para crear una Movies 
     // Puede usar ../models/actorPojo para crear un Actor
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+    res = movieModels.createMovie(req)
+    return res;
 
 }
 
 const updateMovie = (data_movie) => {
     // Puede usar ../models/moviePojo para actualizar una Movies 
     // Puede usar ../models/actorPojo para actualizar un Actor
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+    res = movieModels.updateMovie(req.id)
+    return res;
 
 }
 
 const getMovieBy = (data_movie) => {
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+    res.push(movie);
+    return res;
 }
 
 const updateActors = (data_movie) => {
-    // ...
+    if (!data_movie.req) throw new Error("¡Ups! id of movie not found");
+    res = movieModels.updateActors(req.id)
+    return res;
 }
 
 export default {
--- managers/manager-fs.js
+++ managers/manager-fs.js
@@ -3,10 +3,11 @@ import fs from 'fs';
 // afegiu codi ... (2pto)
 export class ManagerFs {
     constructor(_file) {
-        //...
+    let movies = JSON.parse(fs.readFileSync("./data/movies.json", "utf8"));
+      
     }
     getData() {
-        //...
+    export default movies;
 
     }
 }
